f20c6c11a570ac9a595488b7cf5f3c91493af245
Megan <meganhynek@gmail.com>
2022-11-27 16:04:41 -0600

    sql tests

--- queries.sql
+++ queries.sql
@@ -3,10 +3,15 @@
 -- id is an INT, employer is a VARCHAR, name is a VARCHAR, skill is a VARCHAR, employer_id is an INT, employee_id is an INT
 -- Part 2: Test it with SQL
 SELECT name
-FROM techjobs.employer
+FROM employer
 WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 DROP TABLE job;
 
 -- Part 4: Test it with SQL
+SELECT name
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.job_id IS NOT NULL
+ORDER BY name ASC;
